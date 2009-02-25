6aca96f36460d5c5f092bb34a39d24c987885853
StephaneLenclud <StephaneLenclud@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-02-25 17:03:15 +0000

    Item5389: JQueryLibPlugin adding support for error and highlight buttons.
    git-svn-id: http://svn.foswiki.org/trunk/JQueryLibPlugin@2709 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- pub/System/JQueryLibPlugin/plugins/foswiki/foswiki.js
+++ pub/System/JQueryLibPlugin/plugins/foswiki/foswiki.js
@@ -2,10 +2,28 @@
 //  Foswiki javascript behavior
 //
 
+function fwAddStateActive(obj) 
+	{
+	obj.parents('.fw-buttonset-single:first').find(".fw-button.ui-state-active").removeClass("ui-state-active");
+	if( obj.is('.ui-state-active.fw-button-toggleable, .fw-buttonset-multi .ui-state-active') ){ obj.removeClass("ui-state-active"); }
+	else { obj.addClass("ui-state-active");}	
+	} 	
+
+function fwRemoveStateActive(obj) 
+	{
+	if(! obj.is('.fw-button-toggleable, .fw-buttonset-single .fw-button,  .fw-buttonset-multi .fw-button'))
+		{
+		obj.removeClass("ui-state-active");
+		}		
+	} 
+	
+//
+//Function ready
+//
 $(document).ready(function()
 	{
 	//Hover and click logic for buttons
-	$(".fw-button:not(.ui-state-disabled)")
+	$(".fw-button:not(.ui-state-disabled):not(.ui-state-error):not(.ui-state-highlight)")
 		.hover
 			(
 			function()
@@ -15,28 +33,92 @@ $(document).ready(function()
 			function()
 				{ 
 				$(this).removeClass("ui-state-hover"); 
+				fwRemoveStateActive($(this));				
 				}
 			)
 		.mousedown
 			(
 			function()
 				{
-				$(this).parents('.fw-buttonset-single:first').find(".fw-button.ui-state-active").removeClass("ui-state-active");
-				if( $(this).is('.ui-state-active.fw-button-toggleable, .fw-buttonset-multi .ui-state-active') ){ $(this).removeClass("ui-state-active"); }
-				else { $(this).addClass("ui-state-active");}	
+				fwAddStateActive($(this));
 				}
 			)
 		.mouseup
 			(
 			function()
 				{
-				if(! $(this).is('.fw-button-toggleable, .fw-buttonset-single .fw-button,  .fw-buttonset-multi .fw-button'))
-					{
-					$(this).removeClass("ui-state-active");
-					}
+				fwRemoveStateActive($(this));
+				}
+			);		
+
+	//fw-button with ui-state-error		
+	$(".fw-button:.ui-state-error:not(.ui-state-disabled)")
+		.hover
+			(
+			function()
+				{
+				$(this).addClass("ui-state-hover");
+				$(this).removeClass("ui-state-error"); 				
+				},
+			function()
+				{ 				
+				$(this).removeClass("ui-state-hover"); 	
+				fwRemoveStateActive($(this));
+				$(this).addClass("ui-state-error"); 				
+				}
+			)
+		.mousedown
+			(
+			function()
+				{
+				fwAddStateActive($(this));	
+				$(this).removeClass("ui-state-error");
+				}
+			)
+		.mouseup
+			(
+			function()
+				{
+				fwRemoveStateActive($(this));
+				$(this).addClass("ui-state-error"); 	
 				}
 			);		
 			
+	//fw-button with ui-state-highlight		
+	$(".fw-button:.ui-state-highlight:not(.ui-state-disabled)")
+		.hover
+			(
+			function()
+				{
+				$(this).addClass("ui-state-hover"); 
+				$(this).removeClass("ui-state-highlight");
+				},
+			function()
+				{ 				
+				$(this).removeClass("ui-state-hover"); 	
+				fwRemoveStateActive($(this));
+				$(this).addClass("ui-state-highlight"); 				
+				}
+			)
+		.mousedown
+			(
+			function()
+				{
+				fwAddStateActive($(this));	
+				$(this).removeClass("ui-state-highlight");
+				}
+			)
+		.mouseup
+			(
+			function()
+				{
+				fwRemoveStateActive($(this));
+				$(this).addClass("ui-state-highlight"); 	
+				}
+			);		
+
+			
+									
 	//Here is some code to extend the jQuery offset function so that it can accept parameters
 	//See http://groups.google.com/group/jquery-dev/browse_thread/thread/10fa400d3f9d9521
 	jQuery.fn._offset = jQuery.fn.offset;
