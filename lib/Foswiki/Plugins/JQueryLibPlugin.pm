ae0935d1fbb598b3dbb7f85a86cb40e9c86db0cb
StephaneLenclud <StephaneLenclud@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-02-26 21:15:39 +0000

    Item5389: JQueryLibPlugin proper 0.5 release.
    git-svn-id: http://svn.foswiki.org/trunk/JQueryLibPlugin@2745 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/JQueryLibPlugin.pm
+++ lib/Foswiki/Plugins/JQueryLibPlugin.pm
@@ -51,7 +51,7 @@ our $VERSION = '$Rev: 1340 $';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-our $RELEASE = 'v0.4';
+our $RELEASE = 'v0.5';
 
 # Short description of this plugin
 # One line description, is shown in the %SYSTEMWEB%.TextFormattingRules topic:
